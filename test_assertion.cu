353c558276e868ac8a30c4123d61e8cba2c7a249
hacker <weimin@gapp.nthu.edu.tw>
2020-03-30 15:43:59 +0800

    Update test_assertion.cu

--- test_assertion.cu
+++ test_assertion.cu
@@ -21,3 +21,10 @@ int main(int argc, char* argv[])
 
     return 0;
 }
+
+//[Note]:
+//Assertions are for debugging purposes. 
+//They can affect performance and it is therefore recommended to disable them in production code. 
+//They can be disabled at compile time by defining the NDEBUG preprocessor macro before including assert.h. 
+//Note that expression should not be an expression with side effects (something like (++i > 0), for example), 
+//otherwise disabling the assertion will affect the functionality of the code.
